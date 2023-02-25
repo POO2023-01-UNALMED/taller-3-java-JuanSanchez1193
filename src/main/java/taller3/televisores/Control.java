12392d2f3a2ee51b214423e99b75934792fd9ad7
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2023-02-25 13:22:09 -0500

    test 1

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,40 @@
+package taller3.televisores;
+
+public class Control {
+	private TV tv;
+
+	public Control(TV tv) {
+		super();
+		this.tv = tv;
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+
+	public void enlazar(TV tv) {
+		this.tv=tv;
+		tv.control =this;
+	}
+	public void turnOn() {
+		tv.turnOn();
+	}
+	public void turnOff() {
+		tv.turnOff();
+	}
+	public void volumenUp() {
+		tv.volumenUp();	
+	}
+	public void volumenDown() {
+		tv.volumenDown();
+	}
+	public void setCanal(int canal) {
+		tv.setCanal(canal);
+	}
+
+
+}
